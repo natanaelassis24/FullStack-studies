a758ebd5a4766932519b92686f3a43e78cb3d88b
natanael <natanaelassis77@gmail.com>
2025-08-22 15:55:32 -0300

    0

--- Frontend/note/notes_0.12/my-app/src/components/Tarefa.jsx
+++ Frontend/note/notes_0.12/my-app/src/components/Tarefa.jsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
+import './Tarefa.css'
+
 function Tarefa({texto}){
+
+    const [concluida, setConcluida] = useState(false);
+
+    const alternarConcluida = () => {
+        setConcluida(!concluida);
+    }
+
     return(
-        <li><input type="checkbox"/> {texto} <button>remover</button></li>
+        <li><input type="checkbox" onChange={alternarConcluida}/> <span className={concluida ? 'concluida' : ''}>{texto}</span> <button>remover</button></li>
     )
 }
 export default Tarefa;
