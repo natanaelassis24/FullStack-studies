a4b85f6ba85bc11f38f4a2f2c1abb1b12f31dab2
natanael <natanaelassis77@gmail.com>
2025-08-22 16:27:04 -0300

    0

--- Frontend/note/notes_0.12/my-app/src/App.jsx
+++ Frontend/note/notes_0.12/my-app/src/App.jsx
@@ -1,20 +1,48 @@
+import { useState } from "react";
 import Tarefa from "./components/Tarefa";
 
 function App() {
 
-  const tarefas = [
+  const [tarefas, setTarefas] = useState([
     { id: 1, texto: "Estuda React" },
     { id: 2, texto: "Fazer compras" },
     { id: 3, texto: "Responde e-mails" }
-  ];
+  ]);
+
+  const [novaTarefa, setNovaTarefa] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if(novaTarefa.trim() === '') return;
+
+    const novoId = tarefas.length > 0 ? tarefas[tarefas.length - 1].id + 1 : 1;
+
+    const nova = {
+      id: novoId,
+      texto: novaTarefa.trim()
+    };
+
+    setTarefas([...tarefas, nova]);
+    setNovaTarefa('');
+  }
 
   return (
-    <main>
-      <h1>to-do list app</h1>
+    <main className="container">
+      <h1>To-do List App</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Digite uma nova tarefa"
+          value={novaTarefa}
+          onChange={(e) => setNovaTarefa(e.target.value)}
+        />
+        <button type="submit">Adicionar</button>
+      </form>
       <ul>
-
-        {tarefas.map(tarefa => <Tarefa key={tarefa.id} texto={tarefa.texto} />)}
-
+        {tarefas.map(tarefa => (
+          <Tarefa key={tarefa.id} texto={tarefa.texto} />
+        ))}
       </ul>
     </main>
   );
